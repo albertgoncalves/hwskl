d81337615effb5f37c665f70f4cc2652219cf84c
Albert Goncalves <goncalves.albert@gmail.com>
2020-06-07 10:26:21 -0400

    parens

--- src/BalancedTree.hs
+++ src/BalancedTree.hs
@@ -9,14 +9,14 @@ pad n = concat $ replicate n " "
 showTree :: (Show a) => Tree a -> Int -> String
 showTree Leaf _ = "Leaf"
 showTree (Node h Leaf x Leaf) _ =
-  concat ["Node ", show h, " Leaf ", show x, " Leaf"]
+  concat ["(Node ", show h, " Leaf ", show x, " Leaf)"]
 showTree (Node h l x r) offset =
-  concat ["Node ", show h, indent, l', indent, show x, indent, r']
+  concat ["(Node ", show h, indent, lShow, indent, show x, indent, rShow, ")"]
   where
     offset' = offset + 2
-    l' = showTree l offset'
-    r' = showTree r offset'
     indent = "\n" ++ pad offset'
+    lShow = showTree l offset'
+    rShow = showTree r offset'
 
 instance (Show a) => Show (Tree a) where
   show t = pad n ++ showTree t n
@@ -31,12 +31,13 @@ insert :: (Ord a) => a -> Tree a -> Tree a
 insert x Leaf = Node 0 Leaf x Leaf
 insert x (Node h l x' r)
   | lHeight < rHeight = Node h l' x' r
-  | lHeight > rHeight = Node h l x' (insert x r)
+  | lHeight > rHeight = Node h l x' r'
   | otherwise = Node (height l' + 1) l' x' r
   where
     lHeight = height l
     rHeight = height r
     l' = insert x l
+    r' = insert x r
 
 foldTree :: (Ord a) => [a] -> Tree a
 foldTree = foldr insert Leaf
