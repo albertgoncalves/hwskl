496ce5fffcb32a98298dcaed232fe32a8c4136f2
albertgoncalves <goncalves.albert@gmail.com>
2020-06-11 02:27:55 -0400

    nicer

--- src/Fifo.hs
+++ src/Fifo.hs
@@ -11,19 +11,19 @@ empty = Fifo [] []
 push :: Fifo a -> a -> Fifo a
 push (Fifo input output) x = Fifo (x : input) output
 
-pop :: Fifo a -> (Fifo a, Maybe a)
-pop (Fifo [] []) = (Fifo [] [], Nothing)
-pop (Fifo input (x : output)) = (Fifo input output, Just x)
+pop :: Fifo a -> Maybe (Fifo a, a)
+pop (Fifo [] []) = Nothing
+pop (Fifo input (x : output)) = Just (Fifo input output, x)
 pop (Fifo input []) = pop $ Fifo [] $ reverse input
 
-peek :: Fifo a -> (Fifo a, Maybe a)
-peek (Fifo [] []) = (Fifo [] [], Nothing)
-peek (Fifo input output@(x : _)) = (Fifo input output, Just x)
+peek :: Fifo a -> Maybe (Fifo a, a)
+peek (Fifo [] []) = Nothing
+peek (Fifo input output@(x : _)) = Just (Fifo input output, x)
 peek (Fifo input []) = peek $ Fifo [] $ reverse input
 
 main :: IO ()
 main = do
   print queue
-  print $ pop $ flip push 'd' $ fst $ pop queue
+  print $ pop queue >>= pop . flip push 'd' . fst
   where
     queue = foldl push empty "abc"
