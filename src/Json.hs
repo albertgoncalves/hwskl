61dda8325f1386c36a30ed5e109d513d5e974fc5
Albert Goncalves <goncalves.albert@gmail.com>
2020-06-14 23:35:43 -0400

    adjust some things, still works the same

--- src/Json.hs
+++ src/Json.hs
@@ -1,4 +1,4 @@
-import Control.Applicative ((<|>), Alternative, empty, many)
+import Control.Applicative ((<|>), Alternative, empty, many, some)
 import Data.Char (isDigit, isSpace)
 
 {- NOTE: See `https://www.youtube.com/watch?v=N9RUqGYuGfw`. -}
@@ -33,63 +33,60 @@ instance Alternative Parser where
   empty = Parser (const Nothing)
   (Parser p1) <|> (Parser p2) = Parser $ \xs -> p1 xs <|> p2 xs
 
-parseChar :: Char -> Parser Char
-parseChar x = Parser f
+parseCondition :: (Char -> Bool) -> Parser Char
+parseCondition f = Parser f'
   where
-    f (y : ys)
-      | x == y = Just (ys, y)
-    f _ = Nothing
+    f' (x : xs)
+      | f x = Just (xs, x)
+    f' _ = Nothing
+
+parseChar :: Char -> Parser Char
+parseChar = parseCondition . (==)
 
 parseString :: String -> Parser String
 parseString = traverse parseChar
-
-parseSpan :: (Char -> Bool) -> Parser String
-parseSpan f = Parser $ \xs ->
-  case span f xs of
-    ("", _) -> Nothing
-    (xs', ys) -> Just (ys, xs')
 
 jsonNull :: Parser JsonValue
 jsonNull = JsonNull <$ parseString "null"
 
 jsonBool :: Parser JsonValue
 jsonBool =
-  JsonBool True <$ parseString "true" <|> JsonBool False <$ parseString "false"
+  JsonBool <$> (True <$ parseString "true" <|> False <$ parseString "false")
 
 jsonNumber :: Parser JsonValue
-jsonNumber = JsonNumber . read <$> parseSpan isDigit
+jsonNumber = JsonNumber . read <$> some (parseCondition isDigit)
 
-parseAnyString :: Parser String
-parseAnyString =
-  parseChar '"' *> (parseSpan (/= '"') <|> pure "") <* parseChar '"'
+anyStringLiteral :: Parser String
+anyStringLiteral =
+  parseChar '"' *> many (parseCondition (/= '"')) <* parseChar '"'
 
 jsonString :: Parser JsonValue
-jsonString = JsonString <$> parseAnyString
+jsonString = JsonString <$> anyStringLiteral
 
 sepBy :: Parser a -> Parser b -> Parser [b]
 sepBy sep item = (:) <$> item <*> many (sep *> item) <|> pure []
 
-parseAnySpace :: Parser String
-parseAnySpace = parseSpan isSpace <|> pure ""
+anySpace :: Parser String
+anySpace = many $ parseCondition isSpace
 
-parseCommas :: Parser Char
-parseCommas = parseAnySpace *> parseChar ',' <* parseAnySpace
+comma :: Parser Char
+comma = anySpace *> parseChar ',' <* anySpace
 
 jsonArray :: Parser JsonValue
-jsonArray = JsonArray <$> (lBracket *> sepBy parseCommas jsonValue <* rBracket)
+jsonArray = JsonArray <$> (lBracket *> sepBy comma jsonValue <* rBracket)
   where
-    lBracket = parseChar '[' *> parseAnySpace
-    rBracket = parseAnySpace <* parseChar ']'
+    lBracket = parseChar '[' *> anySpace
+    rBracket = anySpace <* parseChar ']'
 
 jsonObject :: Parser JsonValue
-jsonObject = JsonObject <$> (lBrace *> sepBy parseCommas pair <* rBrace)
+jsonObject = JsonObject <$> (lBrace *> sepBy comma pair <* rBrace)
   where
-    lBrace = parseChar '{' *> parseAnySpace
-    rBrace = parseAnySpace <* parseChar '}'
+    lBrace = parseChar '{' *> anySpace
+    rBrace = anySpace <* parseChar '}'
     pair =
       (\key _ value -> (key, value))
-        <$> parseAnyString
-        <*> (parseAnySpace *> parseChar ':' <* parseAnySpace)
+        <$> anyStringLiteral
+        <*> (anySpace *> parseChar ':' <* anySpace)
         <*> jsonValue
 
 jsonValue :: Parser JsonValue
