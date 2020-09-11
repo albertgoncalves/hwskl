6e7a0f3f571122f9161255ec1890b948b527564e
albertgoncalves <goncalves.albert@gmail.com>
2020-09-10 23:52:51 -0400

    quickSelect!

--- src/QuickSort.hs
+++ src/QuickSort.hs
@@ -4,7 +4,22 @@ quickSort :: Ord a => [a] -> [a]
 quickSort [] = []
 quickSort (x : xs) = quickSort a ++ [x] ++ quickSort b
   where
-    (a, b) = partition (<= x) xs
+    (a, b) = partition (< x) xs
+
+quickSelect :: Ord a => [a] -> Int -> Maybe a
+quickSelect [] _ = Nothing
+quickSelect (x : xs) k
+  | k < l = quickSelect a k
+  | k > l = quickSelect b (k - l - 1)
+  | otherwise = Just x
+  where
+    (a, b) = partition (< x) xs
+    l = length a
 
 main :: IO ()
-main = print $ quickSort [2, 1, 6, 4, 8, 7, 10 :: Int]
+main = do
+  print $ quickSort xs
+  mapM_ (print . quickSelect xs) [0, n `div` 2, n - 1]
+  where
+    xs = [2, 1, 6, 4, 8, 7, 10 :: Int]
+    n = length xs
