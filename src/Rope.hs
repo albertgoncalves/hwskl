527a2aa51b73ddd0ec8835ed38ed7ccd74e9b294
albertgoncalves <goncalves.albert@gmail.com>
2020-09-16 08:32:18 -0400

    rename type constructors

--- src/Rope.hs
+++ src/Rope.hs
@@ -2,33 +2,33 @@ import Prelude hiding (lookup)
 
 data Rope a
   = Empty
-  | Single a
-  | Append Word (Rope a) (Rope a)
-  deriving (Eq, Show)
+  | Leaf a
+  | Node Word (Rope a) (Rope a)
+  deriving (Show)
 
 getIndex :: Rope a -> Word
 getIndex Empty = 0
-getIndex (Single _) = 1
-getIndex (Append i _ _) = i
+getIndex (Leaf _) = 1
+getIndex (Node i _ _) = i
 
 instance Semigroup (Rope a) where
   l <> Empty = l
   Empty <> r = r
-  l <> r = Append (getIndex l + getIndex r) l r
+  l <> r = Node (getIndex l + getIndex r) l r
 
 instance Monoid (Rope a) where
   mempty = Empty
 
 toList :: Rope a -> [a]
 toList Empty = []
-toList (Single x) = [x]
-toList (Append _ l r) = toList l ++ toList r
+toList (Leaf x) = [x]
+toList (Node _ l r) = toList l ++ toList r
 
 lookup :: Rope a -> Word -> Maybe a
 lookup xs i = f xs (i + 1)
   where
-    f (Single x) 1 = Just x
-    f (Append _ l r) i'
+    f (Leaf x) 1 = Just x
+    f (Node _ l r) i'
       | j < i' = f r (i' - j)
       | otherwise = f l i'
       where
@@ -36,13 +36,13 @@ lookup xs i = f xs (i + 1)
     f _ _ = Nothing
 
 insert :: Rope a -> a -> Word -> Rope a
-insert Empty x _ = Single x
-insert xs@(Single _) x 0 = Append 2 (Single x) xs
-insert xs@(Single _) x 1 = Append 2 xs (Single x)
-insert xs@(Single _) _ _ = xs
-insert (Append j l r) x i
-  | i < l' = Append j' (insert l x i) r
-  | otherwise = Append j' l (insert r x $ i - l')
+insert Empty x _ = Leaf x
+insert xs@(Leaf _) x 0 = Node 2 (Leaf x) xs
+insert xs@(Leaf _) x 1 = Node 2 xs (Leaf x)
+insert xs@(Leaf _) _ _ = xs
+insert (Node j l r) x i
+  | i < l' = Node j' (insert l x i) r
+  | otherwise = Node j' l (insert r x $ i - l')
   where
     j' = j + 1
     l' = getIndex l
@@ -51,7 +51,6 @@ main :: IO ()
 main = do
   print xs
   print $ lookup xs 0
-  mapM_ (print . toList) xs'
+  mapM_ (print . toList . insert xs 'f') [0 .. 5]
   where
-    xs = mconcat $ map Single "abcde"
-    xs' = map (insert xs 'f') [0 .. 5]
+    xs = insert (mconcat $ map Leaf "abde") 'c' 2
