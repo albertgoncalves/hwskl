96f9e722daba71e1774f89f3236663d76996bb2d
Albert Goncalves <goncalves.albert@gmail.com>
2020-09-16 20:58:27 -0400

    seems like this implements delete

--- src/Rope.hs
+++ src/Rope.hs
@@ -2,61 +2,61 @@ import Control.Monad (foldM, (>=>))
 import Prelude hiding (lookup)
 
 data Rope a
-  = Empty
-  | Leaf a
+  = Leaf a
   | Node Word (Rope a) (Rope a)
   deriving (Show)
 
 getIndex :: Rope a -> Word
-getIndex Empty = 0
 getIndex (Leaf _) = 1
 getIndex (Node i _ _) = i
 
-instance Semigroup (Rope a) where
-  l <> Empty = l
-  Empty <> r = r
-  l <> r = Node (getIndex l + getIndex r) l r
-
-instance Monoid (Rope a) where
-  mempty = Empty
-
 toList :: Rope a -> [a]
-toList Empty = []
 toList (Leaf x) = [x]
 toList (Node _ l r) = toList l ++ toList r
 
 lookup :: Rope a -> Word -> Maybe a
-lookup xs i = f xs (i + 1)
+lookup (Leaf x) 0 = Just x
+lookup (Node _ l r) i'
+  | j <= i' = lookup r (i' - j)
+  | otherwise = lookup l i'
   where
-    f (Leaf x) 1 = Just x
-    f (Node _ l r) i'
-      | j < i' = f r (i' - j)
-      | otherwise = f l i'
-      where
-        j = getIndex l
-    f _ _ = Nothing
+    j = getIndex l
+lookup _ _ = Nothing
 
 insert :: Rope a -> a -> Word -> Maybe (Rope a)
-insert Empty x _ = Just $ Leaf x
 insert xs@(Leaf _) x 0 = Just $ Node 2 (Leaf x) xs
 insert xs@(Leaf _) x 1 = Just $ Node 2 xs (Leaf x)
 insert (Leaf _) _ _ = Nothing
 insert (Node j l r) x i
   | i < l' = insert l x i >>= \xs -> Just $ Node j' xs r
-  | otherwise = insert r x (i - l') >>= \xs -> Just $ Node j' l xs
+  | otherwise = insert r x (i - l') >>= Just . Node j' l
   where
     j' = j + 1
+    l' = getIndex l
+
+delete :: Rope a -> Word -> Maybe (Rope a)
+delete (Leaf _) _ = Nothing
+delete (Node _ (Leaf _) r) 0 = Just r
+delete (Node _ l (Leaf _)) i
+  | getIndex l == i = Just l
+delete (Node j l r) i
+  | i < l' = delete l i >>= \xs -> Just $ Node j' xs r
+  | otherwise = delete r (i - l') >>= Just . Node j' l
+  where
+    j' = j - 1
     l' = getIndex l
 
 main :: IO ()
 main = do
   print xs
-  maybe (return ()) (mapM_ print) $
-    mapM ((\i -> xs >>= \xs' -> insert xs' ' ' i) >=> Just . toList) ns
+  print' $ mapM ((\i -> xs >>= \xs' -> insert xs' '-' i) >=> toList') ns
+  print' $ mapM ((\i -> xs >>= \xs' -> delete xs' i) >=> toList') ns
   where
+    toList' = Just . toList
+    print' = maybe (return ()) (mapM_ print)
     xs =
       foldM
         (\xs' (x, i) -> insert xs' x i)
-        (mconcat $ map Leaf "dc")
-        [('a', 2), ('b', 2), ('e', 0), ('f', 0)]
-    ns = [0 .. 6]
+        (Leaf 'd')
+        [('c', 0), ('b', 0), ('e', 3), ('a', 0), ('f', 5)]
+    ns = [0 .. 5]
