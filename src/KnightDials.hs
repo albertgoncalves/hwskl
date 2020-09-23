468383792683fbd1576dd887d17879f0a93259f3
albertgoncalves <goncalves.albert@gmail.com>
2020-09-23 00:10:18 -0400

    clean-up

--- src/KnightDials.hs
+++ src/KnightDials.hs
@@ -17,25 +17,24 @@ move (Pos 1) = [Pos 6, Pos 8]
 move (Pos 2) = [Pos 7, Pos 9]
 move (Pos 3) = [Pos 4, Pos 8]
 move (Pos 4) = [Pos 3, Pos 9, Pos 0]
-move (Pos 5) = []
 move (Pos 6) = [Pos 1, Pos 7, Pos 0]
 move (Pos 7) = [Pos 2, Pos 6]
 move (Pos 8) = [Pos 1, Pos 3]
 move (Pos 9) = [Pos 2, Pos 4]
-move _ = undefined
+move _ = []
 
 type T = M.Map (Pos, Step) Word
 
 solve :: (T, Word) -> (Pos, Step) -> (T, Word)
-solve (m, v) x = case M.lookup x m of
+solve (m, v) k = case M.lookup k m of
   Just v' -> (m, v + v')
-  Nothing -> (M.insert x v' m', v + v')
+  Nothing -> (M.insert k v' m', v + v')
     where
-      (k, s) = getStep <$> x
+      (p, s) = getStep <$> k
       (m', v') =
         if s == 0
           then (m, 1)
-          else foldl' solve (m, 0) $ zip (move k) (repeat $ Step $ s - 1)
+          else foldl' solve (m, 0) $ zip (move p) (repeat $ Step $ s - 1)
 
 main :: IO ()
 main = print $ snd $ solve (M.empty, 0) (Pos 6, Step 32)
