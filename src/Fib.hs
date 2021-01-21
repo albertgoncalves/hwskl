8d9b5e4ef66b76f26ce4da7ccf27a1f350e58f36
albertgoncalves <goncalves.albert@gmail.com>
2021-01-20 22:58:31 -0500

    canon

--- src/Fib.hs
+++ src/Fib.hs
@@ -3,5 +3,10 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n - 2) + fib (n - 1)
 
+fibs :: [Word]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
 main :: IO ()
-main = print $ fib 5
+main = do
+  print $ fib 5
+  print $ take 6 fibs
