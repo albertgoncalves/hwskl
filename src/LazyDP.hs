162e07dd8ed418b3888eafb230667f482bd977ce
Albert Goncalves <goncalves.albert@gmail.com>
2021-01-24 11:49:12 -0500

    lazy arrays!

--- src/LazyDP.hs
+++ src/LazyDP.hs
@@ -1,18 +1,30 @@
+import Data.Array (Array, bounds, listArray, (!))
 import Data.List (sort)
 
 -- NOTE: See `https://www.youtube.com/watch?v=LjrCckaHjB0`.
-dp :: [Int] -> [Int]
-dp xs = 1 : map f (tail xs)
+dpList :: [Int] -> [Int]
+dpList xs = 1 : map f (tail xs)
   where
     f x =
       sum $
         map snd $
           takeWhile (\(x', _) -> x' < x) $
             dropWhile (\(x', _) -> 3 < (x - x')) $
-              zip xs $ dp xs
+              zip xs $ dpList xs
+
+dpArray :: Array Int Int -> Array Int Int
+dpArray xs = listArray (bounds xs) (1 : map f [1 ..])
+  where
+    f i =
+      sum
+        . map (dpArray xs !)
+        . takeWhile (\j -> xs ! i - xs ! j <= 3)
+        $ [i - 1, i - 2 .. 0]
 
 main :: IO ()
-main =
-  print $
-    last $
-      dp $ sort $ 0 : 22 : [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4 :: Int]
+main = do
+  print $ last $ dpList xs
+  print $ (! n) $ dpArray $ listArray (0, n) xs
+  where
+    xs = sort $ 0 : 22 : [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4 :: Int]
+    n = length xs - 1
