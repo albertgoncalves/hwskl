84cf0d91e62ad2cb118077f3aa2ffa414095527d
albertgoncalves <goncalves.albert@gmail.com>
2021-04-06 21:43:31 -0400

    tco

--- src/Fib.hs
+++ src/Fib.hs
@@ -3,10 +3,19 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n - 2) + fib (n - 1)
 
+fibTco :: Word -> Word
+fibTco = f 0 1
+  where
+    f :: Word -> Word -> Word -> Word
+    f x _ n
+      | n <= 0 = x
+    f a b n = f (a + b) a (n - 1)
+
 fibs :: [Word]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 main :: IO ()
 main = do
   print $ fib 5
+  print $ fibTco 5
   print $ take 6 fibs
