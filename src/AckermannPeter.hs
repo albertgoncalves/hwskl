8becc2eeb53629b86a5e1cc038b2fb8f6ba9d576
albertgoncalves <goncalves.albert@gmail.com>
2021-04-17 23:15:10 -0400

    more ackermann iterations

--- src/AckermannPeter.hs
+++ src/AckermannPeter.hs
@@ -4,10 +4,4 @@ ackermannPeter m 0 = ackermannPeter (m - 1) 1
 ackermannPeter m n = ackermannPeter (m - 1) $ ackermannPeter m (n - 1)
 
 main :: IO ()
-main =
-  mapM_
-    print
-    [ ackermannPeter 1 9,
-      ackermannPeter 2 9,
-      ackermannPeter 3 9
-    ]
+main = mapM_ (print . ackermannPeter 3) [0 .. 9]
