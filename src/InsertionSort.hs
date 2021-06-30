78c5191acb8f56e58781fc7629a54a281e708f79
albertgoncalves <goncalves.albert@gmail.com>
2021-06-30 19:22:36 -0400

    impl insertion sort

---
+++ src/InsertionSort.hs
@@ -0,0 +1,11 @@
+insert :: Ord a => a -> [a] -> [a]
+insert x' [] = [x']
+insert x' xs'@(x:xs)
+  | x' < x = x':xs'
+  | otherwise = x : insert x' xs
+
+insertionSort :: Ord a => [a] -> [a]
+insertionSort = foldr insert []
+
+main :: IO ()
+main = print $ insertionSort [2, 5, 1, 7, 0, 6, 8, 9, 4, 3 :: Int]
