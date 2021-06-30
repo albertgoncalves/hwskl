cd10de4e0af1b5deddbb9feea322c5fc07d5851a
albertgoncalves <goncalves.albert@gmail.com>
2021-06-30 19:22:49 -0400

    update merge sort

--- src/MergeSort.hs
+++ src/MergeSort.hs
@@ -1,3 +1,10 @@
+split :: [a] -> ([a], [a])
+split [] = ([], [])
+split [x] = ([x], [])
+split (l:r:xs) = (l:ls, r:rs)
+  where
+    (ls, rs) = split xs
+
 merge :: Ord a => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
@@ -8,9 +15,9 @@ merge xs'@(x : xs) ys'@(y : ys)
 mergeSort :: Ord a => [a] -> [a]
 mergeSort [] = []
 mergeSort [a] = [a]
-mergeSort xs = merge (mergeSort l) (mergeSort r)
+mergeSort xs = merge (mergeSort ls) (mergeSort rs)
   where
-    (l, r) = splitAt (length xs `div` 2) xs
+    (ls, rs) = split xs
 
 main :: IO ()
 main = print $ mergeSort [5, 7, 4, 8, 2, 1 :: Int]
