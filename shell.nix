684d0a1cef6625d46c8315b92928969e9d6d2bf0
albertgoncalves <goncalves.albert@gmail.com>
2021-07-24 10:30:17 -0400

    link with lld

--- shell.nix
+++ shell.nix
@@ -1,14 +1,22 @@
 with import <nixpkgs> {};
-mkShell {
+mkShell.override { stdenv = llvmPackages_12.stdenv; } {
     buildInputs = [
         ghc
         glibcLocales
+        gmp
         hlint
+        libffi
+        llvmPackages_12.lld
         ormolu
         parallel
         shellcheck
     ];
+    APPEND_LIBRARY_PATH = lib.makeLibraryPath [
+        gmp
+        libffi
+    ];
     shellHook = ''
+        export LD_LIBRARY_PATH="$APPEND_LIBRARY_PATH:$LD_LIBRARY_PATH"
         . .shellhook
     '';
 }
