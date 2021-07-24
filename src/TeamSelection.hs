5d16e7c87e7d764aded81b81bc30837f4ea03826
Albert Goncalves <goncalves.albert@gmail.com>
2021-07-24 11:13:13 -0400

    add examples

--- src/TeamSelection.hs
+++ src/TeamSelection.hs
@@ -1,6 +1,16 @@
 import Data.List (subsequences)
 
 {- NOTE: See `https://codeforces.com/gym/102646/problem/D`. -}
+{- NOTE: $ 5 4
+ -         5 9 10 3 2
+ -         10 10 5 5
+ -       > 215
+ -}
+{- NOTE: $ 7 4
+ -         1 9 3 8 19 3 2
+ -         50 1 9 3
+ -       > 638
+ -}
 
 combinations :: Int -> [a] -> [[a]]
 combinations n = filter ((== n) . length) . subsequences
