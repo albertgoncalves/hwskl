1d3ba24b411c743395b9af2ae7275684fed5a80a
Albert Goncalves <goncalves.albert@gmail.com>
2021-08-07 18:09:32 -0400

    dp with sparse table example

---
+++ src/Bell.hs
@@ -0,0 +1,23 @@
+import Data.Array (Array, array, (!))
+
+-- NOTE: See `https://www.geeksforgeeks.org/bell-numbers-number-of-ways-to-partition-a-set/`.
+-- NOTE: See `https://oeis.org/A000110`.
+
+bell :: Int -> Int
+bell n = loop ! (n, 0)
+  where
+    loop :: Array (Int, Int) Int
+    loop =
+      array ((0, 0), (n + 1, n + 1)) $
+        ((0, 0), 1) :
+          [ ((i, j), f i j)
+            | i <- [1 .. n],
+              j <- [0 .. i]
+          ]
+
+    f :: Int -> Int -> Int
+    f i 0 = loop ! (i - 1, i - 1)
+    f i j = (loop ! (i - 1, j - 1)) + (loop ! (i, j - 1))
+
+main :: IO ()
+main = print $ bell 20 == 51724158235372
