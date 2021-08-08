2be6a9c97df36e0793db089ee289d71a05db01e1
Albert Goncalves <goncalves.albert@gmail.com>
2021-08-08 18:08:19 -0400

    tabular dp vs bruteforce

---
+++ src/CountPairs.hs
@@ -0,0 +1,23 @@
+import Data.Array (Array, array, (!))
+import Data.Word (Word64)
+
+-- NOTE: See `https://www.geeksforgeeks.org/friends-pairing-problem/`.
+
+tabular :: Word64 -> Word64
+tabular n = loop ! n
+  where
+    loop :: Array Word64 Word64
+    loop = array (0, n) [(i, f i) | i <- [0 .. n]]
+
+    f :: Word64 -> Word64
+    f i
+      | i <= 2 = i
+      | otherwise = (loop ! (i - 1)) + ((i - 1) * (loop ! (i - 2)))
+
+bruteForce :: Word64 -> Word64
+bruteForce n
+  | n <= 2 = n
+  | otherwise = (bruteForce $ n - 1) + ((n - 1) * (bruteForce $ n - 2))
+
+main :: IO ()
+main = mapM_ (\f -> print $ f 32) [tabular, bruteForce]
