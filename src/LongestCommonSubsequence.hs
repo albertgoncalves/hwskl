2126b3a4623cad7554c87cf5e07cca1fcd4d84dd
albertgoncalves <goncalves.albert@gmail.com>
2021-12-26 17:03:51 -0500

    tabular impl

--- src/LongestCommonSubsequence.hs
+++ src/LongestCommonSubsequence.hs
@@ -1,12 +1,10 @@
+import Data.Array (Array, array, listArray, (!))
 import Data.Map (Map, empty, insert, lookup)
 import Prelude hiding (lookup)
 
-lcs ::
-  (Eq a, Ord a, Show a) =>
-  Map ([a], [a]) Int ->
-  [a] ->
-  [a] ->
-  (Map ([a], [a]) Int, Int)
+type Memo a = Map ([a], [a]) Int
+
+lcs :: (Eq a, Ord a) => Memo a -> [a] -> [a] -> (Memo a, Int)
 lcs m [] _ = (m, 0)
 lcs m _ [] = (m, 0)
 lcs m ls'@(l : ls) rs'@(r : rs) =
@@ -26,13 +24,39 @@ lcs m ls'@(l : ls) rs'@(r : rs) =
   where
     k = (ls', rs')
 
-main :: IO ()
-main =
-  print $
-    test
-      "the quick brown fox jumps over the lazy dog"
-      "pack my box with five dozen liquor jugs"
-      16
+lcs' :: (Eq a) => [a] -> [a] -> Int
+lcs' ls rs
+  | nL == 0 = 0
+  | nR == 0 = 0
+  | otherwise = t ! (nL, nR)
   where
-    test :: String -> String -> Int -> Bool
-    test a b x = snd (lcs empty a b) == x
+    nL = length ls
+    nR = length rs
+
+    l = listArray (1, nL) ls
+    r = listArray (1, nR) rs
+
+    t :: Array (Int, Int) Int
+    t =
+      array
+        ((0, 0), (nL, nR))
+        [((j, i), f j i) | j <- [0 .. nL], i <- [0 .. nR]]
+
+    f :: Int -> Int -> Int
+    f 0 _ = 0
+    f _ 0 = 0
+    f j i
+      | (l ! j) == (r ! i) = t ! (j - 1, i - 1) + 1
+      | otherwise = max (t ! (j, i - 1)) (t ! (j - 1, i))
+
+main :: IO ()
+main = do
+  mapM_
+    ( \f ->
+        print $
+          f
+            "the quick brown fox jumps over the lazy dog"
+            "pack my box with five dozen liquor jugs"
+            == 16
+    )
+    [\xs -> snd . lcs empty xs, lcs']
