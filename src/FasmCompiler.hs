b9efde370aacef3d9d1e59f02877432b3d2b2e11
Albert Goncalves <goncalves.albert@gmail.com>
2022-06-18 21:32:44 -0400

    no need for `Data.Text`

--- src/FasmCompiler.hs
+++ src/FasmCompiler.hs
@@ -1,24 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import Data.Char (ord)
 import Data.List (elemIndex, foldl', intercalate)
-import Data.Text (Text, pack, unpack)
-import Data.Text.IO (putStrLn)
 import Text.Printf (printf)
-import Prelude hiding (putStrLn)
 
 data Intrin
   = IntrinPrintf
 
 data Label
-  = LabelFunc Text
+  = LabelFunc String
   | LabelIntrin Intrin
 
 data Expr
   = ExprI64 Int
-  | ExprVar Text
-  | ExprStr Text
-  | ExprAssign Text Expr
+  | ExprVar String
+  | ExprStr String
+  | ExprAssign String Expr
   | ExprCall Label [Expr]
 
 data Reg
@@ -37,30 +32,30 @@ instance Show Reg where
   show RegR8 = "r8"
   show RegR9 = "r9"
 
-type Local = Text
+type Local = String
 
-type Asm = Text
+type Asm = String
 
 argRegs :: [Reg]
 argRegs = [RegRDI, RegRSI, RegRDX, RegR10, RegR8, RegR9]
 
-labelString :: Int -> Text
-labelString = pack . printf "_s%d_"
+labelString :: Int -> String
+labelString = printf "_s%d_"
 
-compile :: [Local] -> [Text] -> Expr -> ([Local], [Text], [Asm])
+compile :: [Local] -> [String] -> Expr -> ([Local], [String], [Asm])
 compile locals strings (ExprI64 i) =
-  (locals, strings, [pack $ printf "\tpush %d" i])
+  (locals, strings, [printf "\tpush %d" i])
 compile locals strings (ExprStr s) =
   case elemIndex s strings of
     Nothing ->
       ( locals,
         s : strings,
-        [pack $ printf "\tpush %s" $ labelString $ length strings]
+        [printf "\tpush %s" $ labelString $ length strings]
       )
     Just n ->
       ( locals,
         strings,
-        [pack $ printf "\tpush %s" $ labelString $ length strings - n]
+        [printf "\tpush %s" $ labelString $ length strings - n]
       )
 compile locals strings (ExprVar v) =
   case elemIndex v locals of
@@ -68,13 +63,13 @@ compile locals strings (ExprVar v) =
     Just n ->
       ( locals,
         strings,
-        [pack $ printf "\tpush qword [rbp - %d]" $ (length locals - n) * 8]
+        [printf "\tpush qword [rbp - %d]" $ (length locals - n) * 8]
       )
 compile locals0 strings0 (ExprAssign var expr) = (var : locals1, strings1, asm)
   where
     (locals1, strings1, asm) = compile locals0 strings0 expr
 compile locals0 strings0 (ExprCall (LabelFunc label) exprs) =
-  (locals1, strings1, "" : pack (printf "\tcall %s" label) : asm)
+  (locals1, strings1, "" : printf "\tcall %s" label : asm)
   where
     (locals1, strings1, asm) = compileArgs argRegs locals0 strings0 exprs
 compile locals0 strings0 (ExprCall (LabelIntrin intrin) exprs) =
@@ -86,11 +81,11 @@ compileIntrin :: Intrin -> [Asm]
 compileIntrin IntrinPrintf = ["", "\tcall printf", "\txor eax, eax"]
 
 compileArgs ::
-  [Reg] -> [Local] -> [Text] -> [Expr] -> ([Local], [Text], [Asm])
+  [Reg] -> [Local] -> [String] -> [Expr] -> ([Local], [String], [Asm])
 compileArgs [] _ _ _ = undefined
 compileArgs _ locals strings [] = (locals, strings, [])
 compileArgs (reg : regs) locals0 strings0 (expr : exprs) =
-  (locals2, strings2, asm1 ++ [pack $ printf "\tpop %s" $ show reg] ++ asm0)
+  (locals2, strings2, asm1 ++ [printf "\tpop %s" $ show reg] ++ asm0)
   where
     (locals1, strings1, asm0) = compile locals0 strings0 expr
     (locals2, strings2, asm1) = compileArgs regs locals1 strings1 exprs
@@ -98,12 +93,12 @@ compileArgs (reg : regs) locals0 strings0 (expr : exprs) =
 dataHeader :: [Asm]
 dataHeader = ["section '.data' writeable"]
 
-compileStrings :: [Text] -> [Asm]
+compileStrings :: [String] -> [Asm]
 compileStrings =
   zipWith
-    ((pack .) . (printf "\t%s db %s,0" . labelString))
+    (printf "\t%s db %s,0" . labelString)
     [0 ..]
-    . map (pack . intercalate "," . map (show . ord) . unpack)
+    . map (intercalate "," . map (show . ord))
     . reverse
 
 fileHeader :: [Asm]
@@ -130,8 +125,7 @@ funcFooter =
 instHeader :: [Asm]
 instHeader =
   [ "section '.text' executable",
-    "_start:",
-    ""
+    "_start:"
   ]
 
 instFooter :: [Asm]
@@ -157,7 +151,7 @@ compileAll exprs =
       instFooter
     ]
   where
-    f :: ([Local], [Text], [Asm]) -> Expr -> ([Local], [Text], [Asm])
+    f :: ([Local], [String], [Asm]) -> Expr -> ([Local], [String], [Asm])
     f (locals0, strings0, asm0) expr = (locals1, strings1, asm1 ++ asm0)
       where
         (locals1, strings1, asm1) = compile locals0 strings0 expr
