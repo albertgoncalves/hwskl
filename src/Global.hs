747c4dc3bddefca0fbbc902ca2185ac08673494b
Albert Goncalves <goncalves.albert@gmail.com>
2022-06-26 13:01:35 -0400

    global mutable variable example

---
+++ src/Global.hs
@@ -0,0 +1,22 @@
+import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
+import System.IO.Unsafe (unsafePerformIO)
+
+counter :: IORef Int
+{-# NOINLINE counter #-}
+counter = unsafePerformIO $ newIORef 0
+
+getCounter :: IO Int
+getCounter = do
+  c <- readIORef counter
+  modifyIORef' counter succ
+  return c
+
+f :: Show a => a -> IO String
+f x = (show x ++) . show <$> getCounter
+
+loop :: IO a -> Int -> IO ()
+loop _ 0 = return ()
+loop f' n = f' >> loop f' (n - 1)
+
+main :: IO ()
+main = loop (putStrLn =<< f (1 :: Int)) 3
