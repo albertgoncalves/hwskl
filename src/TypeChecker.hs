5c7de47e8dc672dcfcf518639c0fe20a991e0ce5
albertgoncalves <goncalves.albert@gmail.com>
2023-01-19 08:11:15 -0500

    big re-factor, simplified a little bit

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,6 +1,6 @@
 import Control.Monad (void)
 import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
-import Data.List (foldl', sortOn)
+import Data.List (foldl')
 import qualified Data.Map as M
 import Text.ParserCombinators.ReadP
   ( ReadP,
@@ -182,129 +182,130 @@ parse = many1 stmt <* token eof
     stmt :: ReadP Stmt
     stmt = bind <++ (StmtEffect <$> expr)
 
-exprCollect :: Int -> Expr -> (Int, [(Expr, Type)], Type)
-exprCollect k (ExprInt _) = (k, [], TypeInt)
-exprCollect k (ExprStr _) = (k, [], TypeStr)
-exprCollect k (ExprVar var) = (k, [], TypeVar var)
-exprCollect _ ExprFunc {} = undefined
-exprCollect k0 (ExprCall call args) =
-  case (call, callType) of
-    (ExprVar var0, TypeVar var1)
-      | var0 == var1 -> (k3, infers3, returnType)
-    _ -> (k3, (call, callType) : infers3, returnType)
-  where
-    (k1, infers1, callType) = exprCollect k0 call
-    (k2, infers2, argTypes) = exprCollects k1 args
-    returnType = TypeVar $ printf "_%d_" k2
-    k3 = succ k2
-    infers3 = (call, TypeFunc argTypes returnType) : infers1 ++ infers2
+deref :: M.Map String Type -> String -> (M.Map String Type, Maybe Type)
+deref bindings0 var0 =
+  case M.lookup var0 bindings0 of
+    Just type0@(TypeVar var1) ->
+      case deref (M.delete var0 bindings0) var1 of
+        (bindings1, Just type1) -> (M.insert var0 type1 bindings1, Just type1)
+        (bindings1, Nothing) -> (M.insert var0 type0 bindings1, Nothing)
+    maybeType -> (bindings0, maybeType)
 
-exprCollects :: Int -> [Expr] -> (Int, [(Expr, Type)], [Type])
-exprCollects = foldl' f . (,[],[])
-  where
-    f :: (Int, [(Expr, Type)], [Type]) -> Expr -> (Int, [(Expr, Type)], [Type])
-    f (k0, infers0, types0) expr = (k1, infers0 ++ infers1, types0 ++ [type1])
-      where
-        (k1, infers1, type1) = exprCollect k0 expr
-
-stmtCollect :: Int -> Stmt -> (Int, [(Expr, Type)])
-stmtCollect k0 (StmtBind var expr) = (k1, (ExprVar var, bindType) : infers)
-  where
-    (k1, infers, bindType) = exprCollect k0 expr
-stmtCollect k0 (StmtEffect expr) = (k1, infers)
-  where
-    (k1, infers, _) = exprCollect k0 expr
-
-stmtCollects :: Int -> [Stmt] -> (Int, [(Expr, Type)])
-stmtCollects = foldl' f . (,[])
-  where
-    f :: (Int, [(Expr, Type)]) -> Stmt -> (Int, [(Expr, Type)])
-    f (k, infers) stmt = (infers ++) <$> stmtCollect k stmt
-
-combineType :: M.Map String Type -> Type -> Type -> M.Map String Type
-combineType bindings type0 type1
-  | type0 == type1 = bindings
-combineType bindings (TypeVar var) type0 =
-  case M.lookup var bindings of
-    Just type1 ->
-      M.insert var type1 $ combineType (M.delete var bindings) type0 type1
-    Nothing -> M.insert var type0 bindings
-combineType bindings type0 (TypeVar var) =
-  case M.lookup var bindings of
-    Just type1 ->
-      M.insert var type1 $ combineType (M.delete var bindings) type0 type1
-    Nothing -> M.insert var type0 bindings
-combineType bindings (TypeFunc args0 return0) (TypeFunc args1 return1) =
-  combineType (combineTypes bindings args0 args1) return0 return1
-combineType _ _ _ = undefined
-
-combineTypes :: M.Map String Type -> [Type] -> [Type] -> M.Map String Type
-combineTypes _ [] (_ : _) = undefined
-combineTypes _ (_ : _) [] = undefined
-combineTypes bindings [] [] = bindings
-combineTypes bindings (type0 : types0) (type1 : types1) =
-  combineTypes (combineType bindings type0 type1) types0 types1
-
-intoType :: M.Map String Type -> Expr -> (M.Map String Type, Type)
-intoType _ (ExprInt _) = undefined
-intoType _ (ExprStr _) = undefined
-intoType bindings (ExprVar var) =
-  case M.lookup var bindings of
-    Just varType -> (bindings, varType)
-    Nothing -> (bindings, TypeVar var)
-intoType _ (ExprFunc {}) = undefined
-intoType bindings0 (ExprCall call args) =
+intoType :: Int -> M.Map String Type -> Expr -> (Int, M.Map String Type, Type)
+intoType k bindings (ExprInt _) = (k, bindings, TypeInt)
+intoType k bindings (ExprStr _) = (k, bindings, TypeStr)
+intoType k bindings0 (ExprVar var0) =
+  case deref bindings0 var0 of
+    (bindings1, Just varType) -> (k, bindings1, varType)
+    (bindings1, Nothing) -> (k, bindings1, TypeVar var0)
+intoType _ _ (ExprFunc {}) = undefined
+intoType k0 bindings0 (ExprCall call args) =
   case callType of
     TypeFunc argTypes returnType ->
-      let bindings2 = combineExprs bindings1 args argTypes
-       in (bindings2, returnType)
+      let (k2, bindings2) = exprCombines k1 bindings1 args argTypes
+       in (k2, bindings2, returnType)
+    TypeVar _ ->
+      let (k2, bindings2, argTypes) = intoTypes k1 bindings1 args
+          returnType = TypeVar $ printf "_%d_" k2
+          (k3, bindings3) =
+            typeCombine (succ k2) bindings2 callType $
+              TypeFunc argTypes returnType
+       in (k3, bindings3, returnType)
     _ -> undefined
   where
-    (bindings1, callType) = intoType bindings0 call
+    (k1, bindings1, callType) = intoType k0 bindings0 call
 
-combineExpr :: M.Map String Type -> Expr -> Type -> M.Map String Type
-combineExpr bindings0 expr = combineType bindings1 exprType
+intoTypes ::
+  Int -> M.Map String Type -> [Expr] -> (Int, M.Map String Type, [Type])
+intoTypes k0 bindings0 =
+  foldr
+    ( \expr (k1, bindings1, types1) ->
+        (: types1) <$> intoType k1 bindings1 expr
+    )
+    (k0, bindings0, [])
+
+stmtCheck :: Int -> M.Map String Type -> Stmt -> (Int, M.Map String Type)
+stmtCheck k0 bindings0 (StmtBind var expr) =
+  case M.lookup var bindings0 of
+    Just exprType0 -> typeCombine k1 bindings1 exprType0 exprType1
+    Nothing -> (k1, M.insert var exprType1 bindings1)
   where
-    (bindings1, exprType) = intoType bindings0 expr
+    (k1, bindings1, exprType1) = intoType k0 bindings0 expr
+stmtCheck k0 bindings0 (StmtEffect expr) = (k1, bindings1)
+  where
+    (k1, bindings1, _) = intoType k0 bindings0 expr
 
-combineExprs :: M.Map String Type -> [Expr] -> [Type] -> M.Map String Type
-combineExprs _ [] (_ : _) = undefined
-combineExprs _ (_ : _) [] = undefined
-combineExprs bindings [] [] = bindings
-combineExprs bindings (expr0 : exprs0) (type0 : types0) =
-  combineExprs (combineExpr bindings expr0 type0) exprs0 types0
+stmtChecks :: Int -> M.Map String Type -> [Stmt] -> (Int, M.Map String Type)
+stmtChecks k bindings = foldl' (uncurry stmtCheck) (k, bindings)
+
+typeCombine ::
+  Int -> M.Map String Type -> Type -> Type -> (Int, M.Map String Type)
+typeCombine k bindings type0 type1
+  | type0 == type1 = (k, bindings)
+typeCombine k0 bindings0 (TypeVar var) type0 =
+  case deref bindings0 var of
+    (bindings1, Just type1) ->
+      M.insert var type1
+        <$> typeCombine k0 (M.delete var bindings1) type0 type1
+    (bindings1, Nothing) -> (k0, M.insert var type0 bindings1)
+typeCombine k0 bindings0 type0 (TypeVar var) =
+  case deref bindings0 var of
+    (bindings1, Just type1) ->
+      M.insert var type1
+        <$> typeCombine k0 (M.delete var bindings1) type0 type1
+    (bindings1, Nothing) -> (k0, M.insert var type0 bindings1)
+typeCombine k0 bindings0 (TypeFunc args0 return0) (TypeFunc args1 return1) =
+  typeCombine k1 bindings1 return0 return1
+  where
+    (k1, bindings1) = typeCombines k0 bindings0 args0 args1
+typeCombine _ _ _ _ = undefined
+
+typeCombines ::
+  Int -> M.Map String Type -> [Type] -> [Type] -> (Int, M.Map String Type)
+typeCombines _ _ types0 types1
+  | length types0 /= length types1 = undefined
+typeCombines k bindings types0 types1 =
+  foldl' (uncurry . uncurry typeCombine) (k, bindings) $ zip types0 types1
+
+exprCombine ::
+  Int -> M.Map String Type -> Expr -> Type -> (Int, M.Map String Type)
+exprCombine k0 bindings0 expr = typeCombine k1 bindings1 exprType
+  where
+    (k1, bindings1, exprType) = intoType k0 bindings0 expr
+
+exprCombines ::
+  Int -> M.Map String Type -> [Expr] -> [Type] -> (Int, M.Map String Type)
+exprCombines _ _ exprs types
+  | length exprs /= length types = undefined
+exprCombines k bindings exprs types =
+  foldl' (uncurry . uncurry exprCombine) (k, bindings) $ zip exprs types
 
 shrinkVar :: M.Map String Type -> String -> (M.Map String Type, Type)
 shrinkVar bindings0 var0 =
-  case M.lookup var0 bindings0 of
-    Just (TypeVar var1) ->
-      let (bindings1, varType) = shrinkVar (M.delete var0 bindings0) var1
-       in (M.insert var0 varType bindings1, varType)
-    Just (TypeFunc argTypes0 returnType0) ->
-      let (bindings1, argTypes1) =
-            shrinkTypes (M.delete var0 bindings0) argTypes0
-          (bindings2, returnType2) = shrinkType bindings1 returnType0
+  case deref bindings0 var0 of
+    (bindings1, Just (TypeFunc argTypes0 returnType0)) ->
+      let (bindings2, argTypes1) =
+            shrinkTypes (M.delete var0 bindings1) argTypes0
+          (bindings3, returnType2) = shrinkType bindings2 returnType0
           funcType = TypeFunc argTypes1 returnType2
-       in (M.insert var0 funcType bindings2, funcType)
-    Just varType -> (bindings0, varType)
-    _ -> undefined
+       in (M.insert var0 funcType bindings3, funcType)
+    (bindings1, Just varType) -> (bindings1, varType)
+    (bindings1, Nothing) -> (bindings1, TypeVar var0)
 
 shrinkType :: M.Map String Type -> Type -> (M.Map String Type, Type)
 shrinkType bindings TypeInt = (bindings, TypeInt)
 shrinkType bindings TypeStr = (bindings, TypeStr)
 shrinkType bindings (TypeVar var) = shrinkVar bindings var
 shrinkType bindings0 (TypeFunc argTypes0 returnType0) =
-  (bindings2, TypeFunc argTypes1 returnType2)
+  TypeFunc argTypes1 <$> shrinkType bindings1 returnType0
   where
     (bindings1, argTypes1) = shrinkTypes bindings0 argTypes0
-    (bindings2, returnType2) = shrinkType bindings1 returnType0
 
 shrinkTypes :: M.Map String Type -> [Type] -> (M.Map String Type, [Type])
-shrinkTypes bindings [] = (bindings, [])
-shrinkTypes bindings0 (type0 : types0) = (bindings2, type1 : types2)
-  where
-    (bindings1, type1) = shrinkType bindings0 type0
-    (bindings2, types2) = shrinkTypes bindings1 types0
+shrinkTypes =
+  foldr
+    (\type0 (bindings0, types0) -> (: types0) <$> shrinkType bindings0 type0)
+    . (,[])
 
 main :: IO ()
 main =
@@ -313,11 +314,8 @@ main =
       . map show
       . M.toList
       . (\bindings -> foldl' ((fst .) . shrinkVar) bindings $ M.keys bindings)
-      . uncurry (combineExprs M.empty)
-      . unzip
-      . sortOn fst
       . snd
-      . stmtCollects 0
+      . stmtChecks 0 M.empty
       . fst
       . head
       . readP_to_S parse
@@ -325,4 +323,4 @@ main =
     "  a = 0\
     \  b = \"a\"\
     \  c = (+ a b)\
-    \  a = ((f1 c) (+ (f0 a) (+ 2 b)) a)"
+    \  d = ((f1 c d) (+ (f0 a) (+ 2 b)) a)"
