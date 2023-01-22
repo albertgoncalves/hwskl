42f9ec9f14767e9735258b2a9a602cb6550288de
albertgoncalves <goncalves.albert@gmail.com>
2023-01-22 17:45:28 -0500

    re-factor with `State` monad

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,10 +1,7 @@
-{-# LANGUAGE LambdaCase #-}
-
-import Control.Monad (void)
+import Control.Monad (void, when)
+import Control.Monad.State (State, execState, modify, state)
 import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
-import Data.List (foldl')
 import qualified Data.Map as M
-import Data.Maybe (mapMaybe)
 import Text.ParserCombinators.ReadP
   ( ReadP,
     char,
@@ -40,6 +37,12 @@ data Type
   | TypeVar String
   | TypeFunc [Type] Type
   deriving (Eq, Ord)
+
+data Compiler = Compiler
+  { compilerK :: Int,
+    compilerBindings :: M.Map String Type,
+    compilerScopes :: [[String]]
+  }
 
 showExpr :: Int -> Expr -> String
 showExpr _ (ExprInt int) = show int
@@ -184,169 +187,193 @@ parse = many1 stmt <* token eof
     stmt :: ReadP Stmt
     stmt = bind <++ (StmtEffect <$> expr)
 
-deref :: M.Map String Type -> String -> (M.Map String Type, Maybe Type)
-deref bindings0 var0 =
-  case M.lookup var0 bindings0 of
-    Just type0@(TypeVar var1) ->
-      case deref (M.delete var0 bindings0) var1 of
-        (bindings1, Just type1) -> (M.insert var0 type1 bindings1, Just type1)
-        (bindings1, Nothing) -> (M.insert var0 type0 bindings1, Just type0)
-    maybeType -> (bindings0, maybeType)
+getBindings :: State Compiler (M.Map String Type)
+getBindings = state $ \c -> (compilerBindings c, c)
+
+getK :: State Compiler Int
+getK = state $ \c -> (compilerK c, c)
+
+setK :: Int -> State Compiler ()
+setK k = modify $ \c -> c {compilerK = k}
+
+nextK :: State Compiler Int
+nextK = do
+  k <- getK
+  setK (succ k)
+  return k
+
+setBindings :: (M.Map String Type -> M.Map String Type) -> State Compiler ()
+setBindings f = modify $ \c -> c {compilerBindings = f $ compilerBindings c}
+
+newScope :: State Compiler ()
+newScope = modify $ \c -> c {compilerScopes = [] : compilerScopes c}
+
+pushScopeVar :: String -> State Compiler ()
+pushScopeVar var = modify $ \c ->
+  case compilerScopes c of
+    (scope : scopes) -> c {compilerScopes = (var : scope) : scopes}
+    _ -> undefined
+
+popScope :: State Compiler [String]
+popScope =
+  state $
+    \c ->
+      (head $ compilerScopes c, c {compilerScopes = tail $ compilerScopes c})
 
 varLabel :: Int -> String
 varLabel = printf "_%d_"
 
-intoType :: Int -> M.Map String Type -> Expr -> (Int, M.Map String Type, Type)
-intoType k bindings (ExprInt _) = (k, bindings, TypeInt)
-intoType k bindings (ExprStr _) = (k, bindings, TypeStr)
-intoType k bindings0 (ExprVar var) =
-  case deref bindings0 var of
-    (bindings1, Just varType) -> (k, bindings1, varType)
-    (bindings1, Nothing) ->
-      let varType = TypeVar $ varLabel k
-       in (succ k, M.insert var varType bindings1, varType)
-intoType _ bindings (ExprFunc args _ _)
-  | any (`M.member` bindings) args = undefined
-intoType k0 bindings0 (ExprFunc args stmts expr) =
-  ( k3,
-    foldr M.delete (shrinkBindings bindings5) vars,
-    TypeFunc argTypes returnType1
-  )
-  where
-    k1 = k0 + length args
-    bindings1 =
-      M.union bindings0 $
-        M.fromList $
-          zip args $
-            map (TypeVar . varLabel) [k0 ..]
-    (k2, bindings2) = stmtChecks k1 bindings1 stmts
-    (k3, bindings3, returnType0) = intoType k2 bindings2 expr
-    (bindings4, argTypes) = shrinkVars bindings3 args
-    (bindings5, returnType1) = shrinkType bindings4 returnType0
-    vars =
-      mapMaybe
-        ( \case
-            StmtBind var _ -> Just var
-            _ -> Nothing
-        )
-        stmts
-intoType k0 bindings0 (ExprCall call args) =
+deref :: String -> State Compiler (Maybe Type)
+deref var0 = do
+  bindings <- getBindings
+  case M.lookup var0 bindings of
+    Just type0@(TypeVar var1) -> do
+      setBindings $ M.delete var0
+      maybeType <- deref var1
+      case maybeType of
+        Just type1 -> do
+          setBindings $ M.insert var0 type1
+          return $ Just type1
+        Nothing -> do
+          setBindings $ M.insert var0 type0
+          return $ Just type0
+    maybeType -> return maybeType
+
+intoType :: Expr -> State Compiler Type
+intoType (ExprInt _) = return TypeInt
+intoType (ExprStr _) = return TypeStr
+intoType (ExprVar var) = do
+  maybeType <- deref var
+  case maybeType of
+    Just varType -> return varType
+    Nothing -> do
+      varType <- TypeVar . varLabel <$> nextK
+      setBindings $ M.insert var varType
+      return varType
+intoType (ExprFunc args stmts expr) = do
+  bindings <- getBindings
+  when (any (`M.member` bindings) args) undefined
+  k <- getK
+  setK (k + length args)
+  setBindings
+    (`M.union` M.fromList (zip args $ map (TypeVar . varLabel) [k ..]))
+  newScope
+  stmtChecks stmts
+  returnType0 <- intoType expr
+  argTypes <- shrinkVars args
+  returnType1 <- shrinkType returnType0
+  shrinkBindings
+  vars <- popScope
+  setBindings $ flip (foldr M.delete) $ args ++ vars
+  return $ TypeFunc argTypes returnType1
+intoType (ExprCall call args) = do
+  callType <- intoType call
   case callType of
-    TypeFunc argTypes returnType ->
-      let (k2, bindings2) = exprCombines k1 bindings1 args argTypes
-       in (k2, bindings2, returnType)
-    TypeVar _ ->
-      let (k2, bindings2, argTypes) = intoTypes k1 bindings1 args
-          returnType = TypeVar $ varLabel k2
-          (k3, bindings3) =
-            typeCombine (succ k2) bindings2 callType $
-              TypeFunc argTypes returnType
-       in (k3, bindings3, returnType)
+    TypeFunc argTypes returnType -> do
+      exprCombines args argTypes
+      return returnType
+    TypeVar _ -> do
+      argTypes <- mapM intoType args
+      returnType <- TypeVar . varLabel <$> nextK
+      typeCombine callType $ TypeFunc argTypes returnType
+      return returnType
     _ -> undefined
-  where
-    (k1, bindings1, callType) = intoType k0 bindings0 call
 
-intoTypes ::
-  Int -> M.Map String Type -> [Expr] -> (Int, M.Map String Type, [Type])
-intoTypes k0 bindings0 =
-  foldr
-    ( \expr (k1, bindings1, types1) ->
-        (: types1) <$> intoType k1 bindings1 expr
-    )
-    (k0, bindings0, [])
+stmtCheck :: Stmt -> State Compiler ()
+stmtCheck (StmtBind var expr) = do
+  bindings <- getBindings
+  case M.lookup var bindings of
+    Just exprType0 -> do
+      exprType1 <- intoType expr
+      typeCombine exprType0 exprType1
+    Nothing -> do
+      exprType1 <- intoType expr
+      setBindings $ M.insert var exprType1
+      pushScopeVar var
+stmtCheck (StmtEffect expr) = void $ intoType expr
 
-stmtCheck :: Int -> M.Map String Type -> Stmt -> (Int, M.Map String Type)
-stmtCheck k0 bindings0 (StmtBind var expr) =
-  case M.lookup var bindings0 of
-    Just exprType0 -> typeCombine k1 bindings1 exprType0 exprType1
-    Nothing -> (k1, M.insert var exprType1 bindings1)
-  where
-    (k1, bindings1, exprType1) = intoType k0 bindings0 expr
-stmtCheck k0 bindings0 (StmtEffect expr) = (k1, bindings1)
-  where
-    (k1, bindings1, _) = intoType k0 bindings0 expr
+stmtChecks :: [Stmt] -> State Compiler ()
+stmtChecks = mapM_ stmtCheck
 
-stmtChecks :: Int -> M.Map String Type -> [Stmt] -> (Int, M.Map String Type)
-stmtChecks k bindings = foldl' (uncurry stmtCheck) (k, bindings)
+typeCombine :: Type -> Type -> State Compiler ()
+typeCombine type0 type1
+  | type0 == type1 = return ()
+typeCombine (TypeVar var) type0 = do
+  maybeType <- deref var
+  case maybeType of
+    Just type1 -> do
+      setBindings $ M.delete var
+      typeCombine type0 type1
+      setBindings $ M.insert var type1
+    Nothing -> do
+      setBindings $ M.insert var type0
+typeCombine type0 (TypeVar var) = do
+  maybeType <- deref var
+  case maybeType of
+    Just type1 -> do
+      setBindings $ M.delete var
+      typeCombine type0 type1
+      setBindings $ M.insert var type1
+    Nothing -> do
+      setBindings $ M.insert var type0
+typeCombine (TypeFunc args0 return0) (TypeFunc args1 return1) = do
+  typeCombines args0 args1
+  typeCombine return0 return1
+typeCombine _ _ = undefined
 
-typeCombine ::
-  Int -> M.Map String Type -> Type -> Type -> (Int, M.Map String Type)
-typeCombine k bindings type0 type1
-  | type0 == type1 = (k, bindings)
-typeCombine k0 bindings0 (TypeVar var) type0 =
-  case deref bindings0 var of
-    (bindings1, Just type1) ->
-      M.insert var type1
-        <$> typeCombine k0 (M.delete var bindings1) type0 type1
-    (bindings1, Nothing) -> (k0, M.insert var type0 bindings1)
-typeCombine k0 bindings0 type0 (TypeVar var) =
-  case deref bindings0 var of
-    (bindings1, Just type1) ->
-      M.insert var type1
-        <$> typeCombine k0 (M.delete var bindings1) type0 type1
-    (bindings1, Nothing) -> (k0, M.insert var type0 bindings1)
-typeCombine k0 bindings0 (TypeFunc args0 return0) (TypeFunc args1 return1) =
-  typeCombine k1 bindings1 return0 return1
-  where
-    (k1, bindings1) = typeCombines k0 bindings0 args0 args1
-typeCombine _ _ _ _ = undefined
+typeCombines :: [Type] -> [Type] -> State Compiler ()
+typeCombines [] [] = return ()
+typeCombines [] _ = undefined
+typeCombines _ [] = undefined
+typeCombines (type0 : types0) (type1 : types1) = do
+  typeCombine type0 type1
+  typeCombines types0 types1
 
-typeCombines ::
-  Int -> M.Map String Type -> [Type] -> [Type] -> (Int, M.Map String Type)
-typeCombines _ _ types0 types1
-  | length types0 /= length types1 = undefined
-typeCombines k bindings types0 types1 =
-  foldl' (uncurry . uncurry typeCombine) (k, bindings) $ zip types0 types1
+exprCombine :: Expr -> Type -> State Compiler ()
+exprCombine expr = (intoType expr >>=) . typeCombine
 
-exprCombine ::
-  Int -> M.Map String Type -> Expr -> Type -> (Int, M.Map String Type)
-exprCombine k0 bindings0 expr = typeCombine k1 bindings1 exprType
-  where
-    (k1, bindings1, exprType) = intoType k0 bindings0 expr
+exprCombines :: [Expr] -> [Type] -> State Compiler ()
+exprCombines [] [] = return ()
+exprCombines [] _ = undefined
+exprCombines _ [] = undefined
+exprCombines (expr0 : exprs0) (type1 : types1) = do
+  exprCombine expr0 type1
+  exprCombines exprs0 types1
 
-exprCombines ::
-  Int -> M.Map String Type -> [Expr] -> [Type] -> (Int, M.Map String Type)
-exprCombines _ _ exprs types
-  | length exprs /= length types = undefined
-exprCombines k bindings exprs types =
-  foldl' (uncurry . uncurry exprCombine) (k, bindings) $ zip exprs types
+shrinkVar :: String -> State Compiler Type
+shrinkVar var = do
+  maybeType <- deref var
+  case maybeType of
+    Just (TypeFunc argTypes0 returnType0) -> do
+      setBindings $ M.delete var
+      argTypes1 <- shrinkTypes argTypes0
+      returnType1 <- shrinkType returnType0
+      let funcType = TypeFunc argTypes1 returnType1
+      setBindings $ M.insert var funcType
+      return funcType
+    Just varType -> return varType
+    Nothing -> return $ TypeVar var
 
-shrinkVar :: M.Map String Type -> String -> (M.Map String Type, Type)
-shrinkVar bindings0 var0 =
-  case deref bindings0 var0 of
-    (bindings1, Just (TypeFunc argTypes0 returnType0)) ->
-      let (bindings2, argTypes1) =
-            shrinkTypes (M.delete var0 bindings1) argTypes0
-          (bindings3, returnType2) = shrinkType bindings2 returnType0
-          funcType = TypeFunc argTypes1 returnType2
-       in (M.insert var0 funcType bindings3, funcType)
-    (bindings1, Just varType) -> (bindings1, varType)
-    (bindings1, Nothing) -> (bindings1, TypeVar var0)
+shrinkVars :: [String] -> State Compiler [Type]
+shrinkVars = mapM shrinkVar
 
-shrinkVars :: M.Map String Type -> [String] -> (M.Map String Type, [Type])
-shrinkVars =
-  foldr
-    (\var (bindings0, types0) -> (: types0) <$> shrinkVar bindings0 var)
-    . (,[])
+shrinkType :: Type -> State Compiler Type
+shrinkType TypeInt = return TypeInt
+shrinkType TypeStr = return TypeStr
+shrinkType (TypeVar var) = shrinkVar var
+shrinkType (TypeFunc argTypes0 returnType0) = do
+  argTypes1 <- shrinkTypes argTypes0
+  returnType1 <- shrinkType returnType0
+  return $ TypeFunc argTypes1 returnType1
 
-shrinkType :: M.Map String Type -> Type -> (M.Map String Type, Type)
-shrinkType bindings TypeInt = (bindings, TypeInt)
-shrinkType bindings TypeStr = (bindings, TypeStr)
-shrinkType bindings (TypeVar var) = shrinkVar bindings var
-shrinkType bindings0 (TypeFunc argTypes0 returnType0) =
-  TypeFunc argTypes1 <$> shrinkType bindings1 returnType0
-  where
-    (bindings1, argTypes1) = shrinkTypes bindings0 argTypes0
+shrinkTypes :: [Type] -> State Compiler [Type]
+shrinkTypes = mapM shrinkType
 
-shrinkTypes :: M.Map String Type -> [Type] -> (M.Map String Type, [Type])
-shrinkTypes =
-  foldr
-    (\type0 (bindings0, types0) -> (: types0) <$> shrinkType bindings0 type0)
-    . (,[])
-
-shrinkBindings :: M.Map String Type -> M.Map String Type
-shrinkBindings bindings =
-  foldl' ((fst .) . shrinkVar) bindings $ M.keys bindings
+shrinkBindings :: State Compiler ()
+shrinkBindings = do
+  bindings <- getBindings
+  _ <- shrinkVars $ M.keys bindings
+  return ()
 
 main :: IO ()
 main =
@@ -355,9 +382,11 @@ main =
         . unlines
         . map show
         . M.toList
-        . shrinkBindings
-        . snd
-        . stmtChecks 0 M.empty
+        . compilerBindings
+        . ( \stmts ->
+              execState (stmtChecks stmts >> shrinkBindings) $
+                Compiler 0 M.empty [[]]
+          )
         . fst
         . head
         . readP_to_S parse
