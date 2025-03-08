dc3db6abc184d3357e669b25443f684dbfc3e5a4
albertgoncalves <goncalves.albert@gmail.com>
2025-03-08 09:37:29 -0500

    slightly generalize function

--- src/GraphSort.hs
+++ src/GraphSort.hs
@@ -3,7 +3,7 @@ import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
 import qualified Data.Set as S
 
-expand :: M.Map String (S.Set String) -> S.Set String -> S.Set String
+expand :: (Ord a) => M.Map a (S.Set a) -> S.Set a -> S.Set a
 expand graph labels =
   case S.minView labels of
     Nothing -> S.empty
@@ -13,7 +13,7 @@ expand graph labels =
             expand (M.delete label graph) $
               S.union remaining parents
 
-reorder :: [(String, S.Set String)] -> [[String]]
+reorder :: (Ord a) => [(a, S.Set a)] -> [[a]]
 reorder [] = []
 reorder (node@(label, parents) : remaining)
   | S.null parents = [label] : reorder (map (S.delete label <$>) remaining)
@@ -34,13 +34,13 @@ main =
           map (expand (M.fromList nodes) . S.singleton) labels
   where
     nodes =
-      [ ("b", S.singleton "a"),
-        ("c", S.singleton "e"),
-        ("a", S.empty),
-        ("f", S.empty),
-        ("d", S.fromList ["a", "c", "f"]),
-        ("e", S.singleton "d"),
-        ("g", S.fromList ["a", "b"]),
-        ("h", S.fromList ["c", "d", "e", "a", "b"])
+      [ ('b', S.singleton 'a'),
+        ('c', S.singleton 'e'),
+        ('a', S.empty),
+        ('f', S.empty),
+        ('d', S.fromList ['a', 'c', 'f']),
+        ('e', S.singleton 'd'),
+        ('g', S.fromList ['a', 'b']),
+        ('h', S.fromList ['c', 'd', 'e', 'a', 'b'])
       ]
     labels = map fst nodes
